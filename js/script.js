1b779447d7c41379ce1c332a1dfa87b14728699b
RyanShaw21 <>
2021-05-04 16:45:41 +0000

    Created index.html, style.css and script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Ryan-Shaw-2 All rights reserved
+//
+// Created by: Ryan-Shaw-2
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function calculateClicked () {
+  // this function calculates area and perimeter
+
+  document.getElementById('area-math').innerHTML = 'The area is: ' + (5 * 3) + 'cm²'
+  document.getElementById('perimeter-math').innerHTML = 'The perimeter is: ' + (2 * (5 + 3)) + 'cm'
+}
